2ddce2504742efa4b4690b1972b303cbf0732fdc
N1xev <alasamole@yahoo.com>
2025-08-16 13:51:51 +0300

    update: Added hostnames of images.

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,48 @@
-import { createMDX } from 'fumadocs-mdx/next';
+import { createMDX } from "fumadocs-mdx/next";
 
 const withMDX = createMDX();
 
 /** @type {import('next').NextConfig} */
 const config = {
-  reactStrictMode: true,
+    reactStrictMode: true,
+    async rewrites() {
+        return [
+            {
+                source: "/docs/:path*.mdx",
+                destination: "/llms.mdx/:path*",
+            },
+        ];
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "github.com",
+                port: "",
+            },
+            {
+                protocol: "https",
+                hostname: "godoc.org",
+                port: "",
+            },
+            {
+                protocol: "https",
+                hostname: "stuff.charm.sh",
+            },
+            {
+                protocol: "https",
+                hostname: "goreportcard.com",
+            },
+            {
+                protocol: "https",
+                hostname: "vhs.charm.sh",
+            },
+            {
+                protocol: "https",
+                hostname: "img.shields.io",
+            },
+        ],
+    },
 };
 
 export default withMDX(config);
