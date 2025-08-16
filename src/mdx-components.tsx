7c347ae46c6ec70ef5009a51d5b2ef28133d8ec1
N1xev <alasamole@yahoo.com>
2025-08-17 00:33:43 +0300

    update: Added new components to the mdx component

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,12 +1,18 @@
 import defaultMdxComponents from "fumadocs-ui/mdx";
 import * as TabsComponents from "fumadocs-ui/components/tabs";
 import type { MDXComponents } from "mdx/types";
+import { CodeBlock, Pre } from "fumadocs-ui/components/codeblock";
 
 // use this function to get MDX components, you will need it for rendering MDX
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
-    return {
-        ...defaultMdxComponents,
-        ...TabsComponents,
-        ...components,
-    };
+  return {
+    ...defaultMdxComponents,
+    pre: ({ ...props }) => (
+      <CodeBlock {...props}>
+        <Pre>{props.children}</Pre>
+      </CodeBlock>
+    ),
+    ...TabsComponents,
+    ...components,
+  };
 }
